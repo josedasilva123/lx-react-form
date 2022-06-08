18c50956c892fe706691ee37b287132140bf312e
Alex Conder <alex.conder@nerdweb.com.br>
2022-06-08 08:46:11 -0300

    feat: index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { useInput } from "./validations/input";
+import { useCheckbox } from './validations/checkbox'
+import { useSelect } from "./validations/select";
+import useForm from "./hooks/Form";
+
+export {useInput, useCheckbox, useSelect, useForm};
+
