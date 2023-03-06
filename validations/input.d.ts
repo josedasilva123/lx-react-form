aede9b7fea36b3331fc5448320d0329b4aa66ca5
Pedro Feitosa <pedrohfeitosa98@gmail.com>
2023-03-06 10:23:14 -0300

    fix: maxLength type

--- validations/input.d.ts
+++ validations/input.d.ts
@@ -1,42 +1,42 @@
 import * as React from "react";
 import { iCustomRule, iMask, iValidation } from "./types/global";
 interface iInputErrorText {
-  required?: string;
-  minLength?: string;
-  maxLength?: string;
-  same?: string;
+    required?: string;
+    minLength?: string;
+    maxLength?: string;
+    same?: string;
 }
 interface iUseInputProps {
-  optional?: boolean;
-  name: string;
-  initialValue?: string;
-  validation?: "email" | "cep" | "senha" | "telefone";
-  mask?: "cep" | "cpf" | "cnpj" | "telefone" | "inteiros";
-  customValidations?: iValidation[];
-  customMask?: iMask;
-  same?: string;
-  minLength?: number;
-  maxLength?: number;
-  errorText?: iInputErrorText;
-  customRule?: iCustomRule;
+    optional?: boolean;
+    name: string;
+    initialValue?: string;
+    validation?: "email" | "cep" | "senha" | "telefone";
+    mask?: "cep" | "cpf" | "cnpj" | "telefone" | "inteiros";
+    customValidations?: iValidation[];
+    customMask?: iMask;
+    same?: string;
+    minLength?: number;
+    maxLength?: number;
+    errorText?: iInputErrorText;
+    customRule?: iCustomRule;
 }
 interface iUseInputInputProps {
-  value: string;
-  name: string;
-  maxLength: number;
-  onChange: (event: React.SyntheticEvent) => void;
-  onKeyUp: () => void;
-  onBlur: () => void;
+    value: string;
+    name: string;
+    maxLength: string;
+    onChange: (event: React.SyntheticEvent) => void;
+    onKeyUp: () => void;
+    onBlur: () => void;
 }
 interface iUseInputReturn {
-  inputProps: iUseInputInputProps;
-  type: "input";
-  value: string;
-  setValue: React.Dispatch<React.SetStateAction<string>>;
-  initialValue: string;
-  error: string | null;
-  setError: React.Dispatch<React.SetStateAction<string | null>>;
-  validate: (disabledErrors?: boolean) => void;
+    inputProps: iUseInputInputProps;
+    type: "input";
+    value: string;
+    setValue: React.Dispatch<React.SetStateAction<string>>;
+    initialValue: string;
+    error: string | null;
+    setError: React.Dispatch<React.SetStateAction<string | null>>;
+    validate: (disabledErrors?: boolean) => void;
 }
 declare type tUseInput = (props: iUseInputProps) => iUseInputReturn;
 /**
